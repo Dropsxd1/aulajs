1226f1338f554c439559de72eda7f3c407c32cc4
Allef C. Nascimento <allef.etp@gmail.com>
2020-02-10 21:39:32 -0300

    remover elementos

--- alunos.js
+++ alunos.js
@@ -1,113 +1,137 @@
-$(document).ready (function (){
 
+$(document).ready(function(){
 
-
-    $("#btn-cadastrar").click (function (){
-
+    $("#btn-cadastrar").click(function () {
 
         var vetor_periodos = ["", "Manhã", "Tarde", "Noite"];
 
         var nome = $("#nome").val();
         var email = $("#email").val();
         var telefone = $("#telefone").val();
+        var periodo = $("#periodo").val()
         var sexo = $("input[name=sexo]:checked").val();
-        var periodo = $("#periodo").val();
+        var errado = false;
 
-        if (nome.trim().length < 3){
-            alert("O campo nome é obrigatório");
+
+        // limpa msg de erros
+        $("#msg-alert").hide();
+        $("#msg-alert").html("");
+        $("input, select").removeClass("erro");        
+        
+
+        if (nome.trim().length < 2)
+        {            
+            $("#msg-alert").append("<li>O campo nome é obrigatorio</li>");
+            $("#msg-alert").fadeIn();
             $("#nome").focus();
-            return false;
-        }   else {
-            
-            var email_regex = /^[0-9a-z\.\_\-]*@[0-9a-z]*\.[a-z.]*$/gm;
-            
-            if (email.search(email_regex) == -1){
-                alert("O formato do email é invalido");
+            $("#nome").addClass("erro");
+            errado = true;
+        }
+
+        if (email == "")
+        {
+            $("#msg-alert").append("<li>O campo email é obrigatorio</li>");
+            $("#msg-alert").fadeIn();
+            $("#email").focus();
+            $("#email").addClass("erro");
+            errado = true;
+        } else 
+        {
+            var email_regex = /^[0-9a-z\.\_\-]*@[0-9a-z.]*$/gm;
+            if (email.search(email_regex) == -1)
+            {
+                $("#msg-alert").append("<li>O formato do email é invalido</li>");
+                $("#msg-alert").fadeIn();
                 $("#email").focus();
-                return false;
+                $("#email").addClass("erro");
+                errado = true;
             }
         }
 
-        if (email == ""){
-            alert("O campo email é obrigatório");
-            $("#email").focus();
-            return false;
-        }
-
-        if (telefone == ""){
-            alert("O campo telefone é obrigatório");
+        if (telefone == "")
+        {
+            $("#msg-alert").append("<li>O campo telefone é obrigatorio</li>");
+            $("#msg-alert").fadeIn();
             $("#telefone").focus();
-            return false;
-        }   else {
-                
+            $("#telefone").addClass("erro");
+            errado = true;
+        } else 
+        {
             var fone_regex = /^\([0-9][0-9]\)[0-9]{4,5}-[0-9]{4}$/gm;
+            
+            if (telefone.search(fone_regex) == -1)
+            {
+                $("#msg-alert").append("<li>O formato do telefone é invalido</li>");
+                $("#msg-alert").fadeIn();
+                $("#telefone").focus();
+                $("#telefone").addClass("erro");
+                errado = true;
+            }
 
-                if (telefone.search(fone_regex) == -1){
-                    alert("O formato do telefone é invalido");
-                    $("#telefone").focus();
-                    return false;
-                }
         }
-               
-        if (sexo == undefined){
-            alert("O campo sexo é obrigatório");
-            return false;
+
+        if (sexo == undefined)
+        {
+            $("#msg-alert").append("<li>O campo sexo é obrigatorio</li>");       
+            $("#msg-alert").fadeIn();     
+            errado = true;
         }
-        
-        if (periodo == "0"){
-            alert("O campo periodo é obrigatório");
+
+        if (periodo == 0)
+        {
+            $("#msg-alert").append("<li>O campo periodo é obrigatorio</li>");
+            $("#msg-alert").fadeIn();
             $("#periodo").focus();
-            return false;
+            $("#periodo").addClass("erro");
+            errado = true;
         }
 
-        //if ternario
-
-        var sexo_rotulo = (sexo == "M")? "Masculino" : "Feminino";
+        if (errado == true)
+        {
+            return false;
+        }
 
         /*
         if (sexo == "M")
         {
             var sexo_rotulo = "Masculino";
-        }   else {
+        } else {
             var sexo_rotulo = "Feminino";
         }*/
+        // if ternario
+        var sexo_rotulo = (sexo == "M")? "Masculino" : "Feminino";
 
         var linha = "<tr>"
-        +"<td>"+ nome +"</td>"
-        +"<td>"+ email +"</td>"
-        +"<td>"+ telefone +"</td>"
-        +"<td>"+ sexo_rotulo +"</td>"
-        +"<td>"+ vetor_periodos[periodo] +"</td>"
-        +"</tr>";
+            +"<td>"+ nome +"</td>"
+            +"<td>"+ email +"</td>"
+            +"<td>"+ telefone +"</td>"
+            +"<td>"+ sexo_rotulo +"</td>"
+            +"<td>"+ vetor_periodos[periodo] + "</td>"
+            +'<td><span class="btn-del">[X]</span></td>'
+            +"</tr>";
 
-    $("#alunos").append(linha);
+        $("#alunos").append(linha);
+        $("input[type=text]").val("");
+        $("select").val("0");
+        $("input[name=sexo]").prop("checked", false);
 
     }); //fim do click
 
-}); // fim do ready
+    // Mascara para telefone
+    $("#telefone").mask("(00)90000-0000");
 
-
-
-
-/*
-function cadastrar()
-{
-    var tbody = document.getElementById("alunos");
-    var nome = document.getElementById("nome").value;
-
-    if (nome == "")
-    {
-        alert("O campo nome é obrigatorio");
-        return false;
-    }
-
-    var linha = "<tr>"
-        +"<td>"+ nome +"</td>"
-        +"<td>pedro@paulo.com</td>"
-        +"<td>11-1234-1234</td>"
-        +"<td>Masculino</td>"
-        +"<td>Noiturno</td>"
-        +"</tr>";
     
-    tbody.innerHTML += linha; 
-}*/
+    //deletar itens da lista
+    $(".btn-del").click(function(){
+        $(this).parent().parent().remove();
+    }); //fim do click
+
+
+    //monitora a adicao de elementos
+    $("#alunos").on("click", ".btn-del", function(){
+        $(this).parent().parent().remove();
+    });
+
+}); // fim ready
+
+
